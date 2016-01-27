e028b9c099934b92f93e21721027c90544ba42c2
Jessica Ziegler <jziegler@lsoc.org>
2016-01-27 12:01:19 -0600

    up to error with case statements

--- rps_pseudo.rb
+++ rps_pseudo.rb
@@ -1,9 +1,25 @@
 win = 0.0
 lose = 0.0
 puts "choose r, p, or s"
-player = gets.chomp.to_s.downcase
+player = gets.chomp.downcase
 puts "The computer chooses:"
 computer = ['r','p','s']
-puts computer.sample
+puts computer.sample.to_s
 
 3.times do 
+	
+end
+case [player, computer]
+
+when ['r', 'r'] ['p','p'] ['s','s']
+	puts "You tie!"
+	win = win + 1.0
+
+when ['r', 's'] ['p','r'] ['s','p']
+	puts "You win!"
+
+
+when ['r', 'p'] ['p', 's'] ['s','r'] 
+	puts "You lose!"
+	lose = lose +1.0
+end
